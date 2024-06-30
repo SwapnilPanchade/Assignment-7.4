b5c090c2d7df094376db032604b695e94e656240
SwapnilPanchade <sspanchade113@gmail.com>
2024-06-30 22:07:22 +0530

    Made some changes in atom-family and did some syntatical suger for it

--- atom-family/src/todos.js
+++ atom-family/src/todos.js
@@ -1,9 +1,23 @@
-export const TODOS = [{
+import { todosAtomFamily } from "../../loadables/src/atoms";
+import { atomFamily } from "recoil";
+import { TODOS } from "./todos";
+
+export const TODOS = [
+  {
     id: 1,
     title: "Go to Gym",
-    description: "Hit the gym from 7-9"
-}, {
+    description: "Hit the gym from 7-9",
+  },
+  {
     id: 2,
     title: "Go to eat food",
-    description: "Eat food from from 9-11"
-},]
+    description: "Eat food from from 9-11",
+  },
+];
+
+export const todosAtomFamily = atomFamily({
+  key: "todosAtomFamily",
+  default: (id) => {
+    return TODOS.find((x) => x.id == id);
+  },
+});
